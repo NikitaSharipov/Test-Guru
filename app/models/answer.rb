247e744ed6a137e27d751cccc4dfa993be8da6b5
NikitaSharipov <sharipovkzn@mail.ru>
2018-09-26 15:57:17 +0300

    Исправления scope validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,15 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
   validates :body, presence: true
-
-  scope  :correct_answer, -> { where(correct: true) }
-
   validate :validate_amount_answers, on: :create
 
+  scope  :right, -> { where(correct: true) }
+
   def validate_amount_answers
-    return if question.answers.length.between?(1,4)
+    return if question.answers.length < 4
     errors.add(:answers, 'Amount of answers to question sholud be from 1 to 4')
   end
 
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   validates :email, presence: true
 
-  scope :test_by_lvl, -> (level_input) { Test.where(level: level_input) }
+#  scope :test_by_lvl, -> (level_input) { Test.where(level: level_input) }
+#  scope :test_by_lvl1, -> (level_input) { tests.where(level: level_input) }
 
-
-#  def test_by_lvl(level_input)
-#    Test.where( level: level_input)
-#  end
+  def test_by_lvl(level_input)
+    tests.by_level(level_input)
+  end
 end
