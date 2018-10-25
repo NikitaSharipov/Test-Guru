0fef484f4160975c00780fbc5f4ef623be839393
NikitaSharipov <sharipovkzn@mail.ru>
2018-10-25 20:15:30 +0300

    14

--- Gemfile
+++ Gemfile
@@ -37,8 +37,13 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+#AUTH
 
 gem 'devise', '~> 4.0'
+
+#NETWORKING
+
+gem 'faraday'
 
 #LOCALIZATION
 
--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,12 +1,16 @@
 class TestPassagesController < ApplicationController
 
-  before_action :set_test_passage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result gist]
 
   def show
 
   end
 
   def result
+
+  end
+
+  def gist
 
   end
 
@@ -19,6 +23,18 @@ class TestPassagesController < ApplicationController
     else
       render :show
     end
+  end
+
+  def gist
+    result = GistQuestionService.new(@test_passage.current_question).call
+
+    flash_options = if result.success?
+      { notice: t('.success') }
+    else
+      { alert: t('.falure') }
+    end
+
+    redirect_to @test_passage, flash_options
   end
 
   private
